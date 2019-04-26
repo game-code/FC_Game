b02307676955ee6d5ee83528e8c10611c214366a
game-code <690242418@qq.com>
2019-04-26 23:54:44 +0800

    更新

--- FcGame/temp/quick-scripts/assets/script/weapon/Zidan.js
+++ FcGame/temp/quick-scripts/assets/script/weapon/Zidan.js
@@ -1,5 +1,5 @@
 (function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/script/weapon/Zidan.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
-cc._RF.push(module, '521f56g7ZZG24sDyJfVvDL8', 'Zidan', __filename);
+cc._RF.push(module, 'b826aFNWKpDM6ff+23vo2AN', 'Zidan', __filename);
 // script/weapon/Zidan.ts
 
 Object.defineProperty(exports, "__esModule", { value: true });
